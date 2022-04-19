a883bc846ef3d6abc3879f2ecbfe4f72f2d53daf
Harshida Shaily <78414863+Harshidashaily@users.noreply.github.com>
2022-04-20 00:16:43 +0530

    Update QUICK_SORT.cpp

--- QUICK_SORT.cpp
+++ QUICK_SORT.cpp
@@ -52,6 +52,13 @@ int main   ()
         cout<<arr[i]<<"   ";
     }
     cout<<"]"<<endl;
+    QUICK_SORT(arr,0,size-1);
+    cout<<"The Elements After Sorting In The Array Is As Follows=[   ";
+    for   (int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<"   ";
+    }
+    cout<<"]"<<endl;
     return 0;
     getchar();
 }
